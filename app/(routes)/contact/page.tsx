7aee05d19fe97671f278d0d8aaf4ea900c03e66f
Sean Coutinho <SeanCoutinho@my.uopeople.edu>
2024-08-29 15:53:02 +0200

    fat: contact form complete

--- app/(routes)/contact/page.tsx
+++ app/(routes)/contact/page.tsx
@@ -2,34 +2,46 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div className="flex justify-between p-12">
-      <div className="w-1/2 p-4">
+    <div className="flex flex-col lg:flex-row justify-between p-6 lg:p-12">
+      {/* Form Section */}
+      <div className="w-full lg:w-1/2 p-4">
         <form className="flex flex-col space-y-4">
-          <input type="text" placeholder="Your Name" className="border p-2" />
-          <input type="email" placeholder="Your Email" className="border p-2" />
-          <input type="email" placeholder="Your Email" className="border p-2" />
-
-          <textarea
+          <input
+            type="text"
+            placeholder="Your Name"
+            className="border p-2 rounded"
+          />
+          <input
+            type="email"
+            placeholder="Your Email"
+            className="border p-2 rounded"
+          />
+          <input
+            type="text"
             placeholder="Your Subject"
-            className="border p-2 h-24"></textarea>
+            className="border p-2 rounded"
+          />
+          <textarea
+            placeholder="Your Message"
+            className="border p-2 h-32 rounded"></textarea>
           <button
             type="submit"
-            className="bg-orange-500 w-fit text-white py-2 px-4 hover:bg-black transition-colors duration-200 ease-in-out">
+            className="bg-orange-500 text-white py-2 px-4 rounded hover:bg-black transition-colors duration-200 ease-in-out w-fit">
             Send message
           </button>
         </form>
       </div>
-      <div className="w-1/2">
-        {/* Embedding Google Maps would require an iframe or using the Google Maps API */}
-        {/* This is just a placeholder */}
-        <div className="h-full flex items-center justify-center">
+
+      {/* Google Maps Section */}
+      <div className="w-full lg:w-1/2 mt-8 lg:mt-0 p-4">
+        <div className="h-64 lg:h-full flex items-center justify-center">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3798.171216598193!2d31.09448257497087!3d-17.830606883134074!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1931bbe4c7e8bef9%3A0xff312419ac55e877!2sRaysun%20Capital!5e0!3m2!1sen!2szw!4v1724938536642!5m2!1sen!2szw"
-            width="600"
-            height="450"
-            allowfullscreen=""
+            width="100%"
+            height="100%"
             loading="lazy"
-            referrerpolicy="no-referrer-when-downgrade"></iframe>
+            referrerpolicy="no-referrer-when-downgrade"
+            className="border rounded-lg shadow-lg"></iframe>
         </div>
       </div>
     </div>
--- components/home/header/page.tsx
+++ components/home/header/page.tsx
@@ -4,21 +4,26 @@ import React, { useState } from "react";
 
 const Header: React.FC = () => {
   const [selectedLink, setSelectedLink] = useState<string>("Home");
-    const [isExpanded, setIsExpanded] = useState(false);
+  const [isExpanded, setIsExpanded] = useState(false);
 
   const handleLinkClick = (link: string) => {
     setSelectedLink(link);
+    setIsExpanded(false); // Close the mobile menu after a link is clicked
+  };
+
+  const toggleMenu = () => {
+    setIsExpanded(!isExpanded);
   };
 
   return (
-    <div className="flex h-24">
+    <div className="relative flex h-24">
       <header className="flex w-full justify-between items-center text-gray-600 sm:py-6">
         <div className="flex-shrink-0 mx-8 font-bold">TBF SERVICES</div>
-        <nav className="items-start px-4 hidden sm:flex font-bold">
-          <ul className="flex items-center space-x-12">
+        <nav className="hidden sm:flex items-center font-bold">
+          <ul className="flex items-center space-x-12 px-12">
             {[
               { name: "Home", href: "/" },
-              { name: "About", href: "about" },
+              { name: "About", href: "/about" },
               { name: "Services", href: "/services" },
               { name: "Products", href: "/products" },
               { name: "Contact", href: "/contact" },
@@ -28,7 +33,7 @@ const Header: React.FC = () => {
                   href={link.href}
                   className={`nav-link ${
                     selectedLink === link.name ? "active" : ""
-                  }  hover:text-[#F5811F]`}
+                  } hover:text-[#F5811F]`}
                   onClick={() => handleLinkClick(link.name)}>
                   {link.name}
                 </a>
@@ -36,72 +41,69 @@ const Header: React.FC = () => {
             ))}
           </ul>
         </nav>
+        <button
+          onClick={toggleMenu}
+          className="sm:hidden px-4 py-2 text-black font-bold">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="size-6">
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+            />
+          </svg>
+        </button>
       </header>
-      <button className="relative flex items-center font-bold w-full text-nowrap px-12 text-black justify-center sm:w-auto bg-[#F5811F] overflow-hidden group">
-        <span className="relative z-10 transition-colors duration-300 ease-in-out group-hover:text-white">
-          Get In Touch
-        </span>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          className="size-4 ml-2 text-white">
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="m8.25 4.5 7.5 7.5-7.5 7.5"
-          />
-        </svg>
-        <span className="absolute inset-0 bg-black transition-transform duration-300 ease-in-out transform -translate-x-full group-hover:translate-x-0"></span>
-      </button>
+
+      <div className="hidden sm:flex">
+        <button className="relative flex items-center font-bold text-nowrap px-12 text-black justify-center bg-[#F5811F] overflow-hidden group">
+          <span className="relative z-10 transition-colors duration-300 ease-in-out group-hover:text-white">
+            Get In Touch
+          </span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="size-4 ml-2 text-white">
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="m8.25 4.5 7.5 7.5-7.5 7.5"
+            />
+          </svg>
+          <span className="absolute inset-0 bg-black transition-transform duration-300 ease-in-out transform -translate-x-full group-hover:translate-x-0"></span>
+        </button>
+      </div>
 
       {isExpanded && (
-        <nav className="lg:hidden">
-          <div className="flex flex-col pt-8 pb-4 space-y-6">
-            <a
-              href="#"
-              title=""
-              className="text-base font-normal text-gray-400 transition-all duration-200 hover:text-white">
-              Products
-            </a>
-            <a
-              href="#"
-              title=""
-              className="text-base font-normal text-gray-400 transition-all duration-200 hover:text-white">
-              Features
-            </a>
-            <a
-              href="#"
-              title=""
-              className="text-base font-normal text-gray-400 transition-all duration-200 hover:text-white">
-              Pricing
-            </a>
-            <a
-              href="#"
-              title=""
-              className="text-base font-normal text-gray-400 transition-all duration-200 hover:text-white">
-              Support
-            </a>
-            <a
-              href="#"
-              title=""
-              className="text-base font-normal text-gray-400 transition-all duration-200 hover:text-white">
-              Login
-            </a>
-
-            <div className="relative inline-flex items-center justify-center group">
-              <div className="absolute transition-all duration-200 rounded-full -inset-px bg-gradient-to-r from-cyan-500 to-purple-500 group-hover:shadow-lg group-hover:shadow-cyan-500/50"></div>
-              <a
-                href="#"
-                title=""
-                className="relative inline-flex items-center justify-center w-full px-6 py-2 text-base font-normal text-white bg-black border border-transparent rounded-full"
-                role="button">
-                Start free trial
-              </a>
-            </div>
-          </div>
+        <nav className="sm:hidden absolute top-full left-0 w-full bg-white shadow-lg z-10">
+          <ul className="flex flex-col space-y-6 p-4">
+            {[
+              { name: "Home", href: "/" },
+              { name: "About", href: "/about" },
+              { name: "Services", href: "/services" },
+              { name: "Products", href: "/products" },
+              { name: "Contact", href: "/contact" },
+            ].map((link) => (
+              <li key={link.name}>
+                <a
+                  href={link.href}
+                  className={`block text-gray-600 hover:text-[#F5811F] ${
+                    selectedLink === link.name ? "font-bold" : ""
+                  }`}
+                  onClick={() => handleLinkClick(link.name)}>
+                  {link.name}
+                </a>
+              </li>
+            ))}
+          </ul>
         </nav>
       )}
     </div>
